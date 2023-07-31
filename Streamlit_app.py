39bfae5d3cd0bb002cd5732349aaf1dadcfa0497
sumanthrancho <140693554+sumanthrancho@users.noreply.github.com>
2023-07-31 22:44:50 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -38,7 +38,7 @@ def main():
             st.error("Wrong PIN. Transaction canceled.")
             return
 
-        while True:
+        else:
             st.write("ATM Operations:")
             st.write("1. Credit (Deposit) Money")
             st.write("2. Debit (Withdraw) Money")
@@ -47,23 +47,23 @@ def main():
             st.write("5. Exit")
 
             choice = st.number_input("Enter your choice (1-5):", min_value=1, max_value=5, step=1)
-
-            if choice == 1:
-                amount = st.number_input("Enter the amount to credit:", step=0.01, format="%.2f")
-                account1.credit(amount)
-            elif choice == 2:
-                amount = st.number_input("Enter the amount to debit:", step=0.01, format="%.2f")
-                account1.debit(amount)
-            elif choice == 3:
-                account1.check_balance()
-            elif choice == 4:
-                new_pin = st.text_input("Enter your new PIN:", type="password")
-                account1.change_pin(new_pin)
-            elif choice == 5:
-                st.write("Thank you for using the ATM. Goodbye!")
-                break
-            else:
-                st.error("Invalid choice. Please try again.")
+            if(st.button('proceed'):
+                if choice == 1:
+                    amount = st.number_input("Enter the amount to credit:", step=0.01, format="%.2f")
+                    account1.credit(amount)
+                elif choice == 2:
+                    amount = st.number_input("Enter the amount to debit:", step=0.01, format="%.2f")
+                    account1.debit(amount)
+                elif choice == 3:
+                    account1.check_balance()
+                elif choice == 4:
+                    new_pin = st.text_input("Enter your new PIN:", type="password")
+                    account1.change_pin(new_pin)
+                elif choice == 5:
+                    st.write("Thank you for using the ATM. Goodbye!")
+                    break
+                else:
+                    st.error("Invalid choice. Please try again.")
 
 if __name__ == "__main__":
     main()
